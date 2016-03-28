6c6e52ce5341a75082fa5429b0123c6ccaada5e5
Jorge Bucaran <j@bucaran.me>
2016-03-29 04:05:29 +0900

    Classical prompt inspired by dartfish.

---
+++ README.md
@@ -0,0 +1,31 @@
+[![Slack Room][slack-badge]][slack-link]
+
+# Dartfish
+
+Classical prompt inspired by dartfish.
+
+![Dartfish]
+
+## Install
+
+With [Fisherman]
+
+```fish
+fisher i dartfish
+```
+
+## Terminal Settings
+
+* [Dartfish.terminal]
+* [Dartfish.itermcolors]
+* Font: 16pt Source Code Pro Medium
+* Non-ASCII Font: 17pt Source Code Pro for [Powerline] [Optional]
+
+[slack-link]: https://fisherman-wharf.herokuapp.com/
+[slack-badge]: https://img.shields.io/badge/slack-join%20the%20chat-00B9FF.svg?style=flat-square
+
+[Fisherman]: https://github.com/fisherman/fisherman
+[Powerline]: https://github.com/powerline/fonts
+[Dartfish]: https://cloud.githubusercontent.com/assets/8317250/14086896/6520e96a-f562-11e5-92c2-557e209c069b.png
+[Dartfish.terminal]: https://github.com/fishery/dartfish/raw/master/Dartfish.terminal
+[Dartfish.itermcolors]: https://github.com/fishery/dartfish/raw/master/Dartfish.itermcolors
---
+++ functions/set_color_custom.fish
@@ -0,0 +1,24 @@
+function set_color_custom
+    set -U fish_color_normal                normal
+    set -U fish_color_command               0fc
+    set -U fish_color_param                 fff
+    set -U fish_color_redirection           fff
+    set -U fish_color_comment               555
+    set -U fish_color_error                 f30
+    set -U fish_color_escape                fc9
+    set -U fish_color_operator              fc9
+    set -U fish_color_end                   fc9
+    set -U fish_color_quote                 ffc
+    set -U fish_color_autosuggestion        555
+    set -U fish_color_valid_path            --underline
+    set -U fish_color_cwd                   0fc
+    set -U fish_color_cwd_root              0fc
+    set -U fish_color_match                 0fc
+    set -U fish_color_search_match          --background=066
+    set -U fish_color_selection             --background=066
+    set -U fish_pager_color_prefix          white
+    set -U fish_pager_color_completion      white -o
+    set -U fish_pager_color_description     white
+    set -U fish_pager_color_progress        fc9
+    set -U fish_color_history_current       fc9
+end

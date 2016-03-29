2fc1e2fea3eb8273336728f9f74138e417899a96
Jorge Bucaran <j@bucaran.me>
2016-03-30 00:37:18 +0900

    Classical prompt inspired by dartfish.

---
+++ README.md
@@ -0,0 +1,29 @@
+[![Slack Room][slack-badge]][slack-link]
+
+# Dartfish
+
+Classical prompt inspired by dartfish.
+
+![Dartfish]
+
+## Install
+
+With [Fisherman]
+
+```fish
+fisher i dartfish
+```
+
+## Terminal Settings
+
+* [Dartfish.terminal]
+* [Dartfish.itermcolors]
+* Font: 16pt Source Code Pro Medium
+
+[slack-link]: https://fisherman-wharf.herokuapp.com/
+[slack-badge]: https://img.shields.io/badge/slack-join%20the%20chat-00B9FF.svg?style=flat-square
+
+[Fisherman]: https://github.com/fisherman/fisherman
+[Dartfish.terminal]: https://github.com/fishery/dartfish/raw/master/Dartfish.terminal
+[Dartfish.itermcolors]: https://github.com/fishery/dartfish/raw/master/Dartfish.itermcolors
+[Dartfish]: https://cloud.githubusercontent.com/assets/8317250/14098420/8ca681e4-f5b6-11e5-818c-25470c07b4a7.png
---
+++ functions/fish_prompt.fish
@@ -0,0 +1,80 @@
+function fish_prompt
+    set -l status_copy $status
+    set -l status_color 0fc
+    set -l root_glyph
+    set -l pwd_info (pwd_info "/")
+
+    if test "$status_copy" -ne 0
+        set status_color f30
+    end
+
+    if pwd_is_home
+        echo_color -o $status_color "⋊> "
+        set root_glyph "~/"
+    else
+        echo_color -o $status_color "⧕ "
+        set root_glyph "/"
+    end
+
+    if test 0 -eq (id -u $USER) -o ! -z "$SSH_CLIENT"
+        echo_color 0fc (host_info "user@host ")
+    end
+
+    echo_color cff $root_glyph
+
+    if test ! -z "$pwd_info[2]"
+        echo_color cff "$pwd_info[2]/"
+    end
+
+    if test ! -z "$pwd_info[1]"
+        echo_color 0fc "$pwd_info[1]"
+    end
+
+    if test ! -z "$pwd_info[3]"
+        echo_color cff "/$pwd_info[3]"
+    end
+
+    if set -l branch_name (git_branch_name)
+        set -l git_glyph " on "
+        set -l branch_glyph
+        set -l branch_color 0fc -o
+
+        if git_is_detached_head
+            set git_glyph " detached "
+        end
+
+        if git_is_touched
+            set branch_color cc3
+
+            if git_is_staged
+                if git_is_dirty
+                    set branch_glyph " ±"
+                else
+                    set branch_glyph " +"
+                end
+            else
+                set branch_glyph " *"
+            end
+        end
+
+        set -l git_ahead (command git rev-list --left-right --count 'HEAD..master' ^ /dev/null | awk '
+            $1 > 0 { printf("↑") }
+            $2 > 0 { printf("↓") }
+        ')
+
+        if git_is_stashed
+            set branch_name "{$branch_name}"
+        end
+
+        echo_color fff "$git_glyph"
+        echo_color $branch_color "$branch_name"
+
+        if test ! -z "$git_ahead"
+            echo_color fff " $git_ahead"
+        end
+
+        echo_color fff "$branch_glyph"
+    end
+
+    echo_color " "
+end
